c3c6467b50a007122e6b5d55985f210dc4f89d41
Joy Nkatha <nkathajoy36@gmail.com>
2023-03-20 20:01:52 +0300

    Data tables

--- resources/views/pickup/viewPickupTimes.blade.php
+++ resources/views/pickup/viewPickupTimes.blade.php
@@ -1,7 +1,5 @@
+@include('common/header')
 <head>
-    @include('common/header')
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js" integrity="sha384-w76AqPfDkMBDXo30jS1Sgez6pr3x5MlQ1ZAGC+nuZB+EYdgRZgiwxhTBTkF7CXvN" crossorigin="anonymous"></script>
     <link href = "//cdn.datatables.net/1.13.4/css/jquery.dataTables.min.css" rel = "stylesheet">
 </head>
 <body>
@@ -9,21 +7,24 @@
 <h3 style="text-align:center;">View Pickup Times</h3>
 
     <table id="pickupView">
-        <tr>
+        <thead>
+        
             <th>Pickup ID No.</th>
             <th>Pickup Location</th>
-            <th>Pickup Date and Time</th>
+            <th>Pickup Date</th>
             <th>Client</th>
 
-        </tr>
-        @foreach($pickupTimes as $pickupTime )
-            <tr>
-                <td>{{$pickupTime->id}}</td>
-                <td>{{$pickupTime->pickupLocation}}</td>
-                <td>{{$pickupTime->pickupDateTime}}</td>
-                <td>{{$pickupTime->users->firstName. ' '.$pickupTime-> users->lastName}}</td>
-            </tr>
-        @endforeach
+        </thead>
+        <tbody>
+            @foreach($pickupTimes as $pickupTime )
+                <tr>
+                    <td>{{$pickupTime->id}}</td>
+                    <td>{{$pickupTime->pickupLocation}}</td>
+                    <td>{{$pickupTime->pickupDateTime}}</td>
+                    <td>{{$pickupTime->users->firstName. ' '.$pickupTime-> users->lastName}}</td>
+                </tr>
+            @endforeach
+        </tbody>
     </table>
 </body>
 <script src="https://code.jquery.com/jquery-3.6.4.min.js" integrity="sha256-oP6HI9z1XaZNBrJURtCoUT5SUnxFr8s3BzRl+cbzUq8=" crossorigin="anonymous"></script>
